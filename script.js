74ea75400dc4aa85cae5d9019ec1c43d9875a752
Kushal S <skushal.mys@gmail.com>
2025-01-22 11:33:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("toss-button").addEventListener("click", function () {
+    const resultElement = document.getElementById("result");
+    const coinElement = document.getElementById("coin");
+
+    //flip coin anime
+
+    resultElement.textContent = "Flipping...";
+    resultElement.style.color = "#00ff85";
+    coinElement.classList.add("flip");
+  
+    // logic 
+    setTimeout(() => {
+      const isHeads = Math.random() > 0.5;
+      coinElement.classList.remove("flip");
+  
+      // result
+      if (isHeads) {
+        coinElement.style.transform = "rotateY(0)";
+        resultElement.textContent = "🎉 It's Heads! You won!";
+        resultElement.style.color = "#00ff85";
+      } else {
+        coinElement.style.transform = "rotateY(180deg)";
+        resultElement.textContent = "💔 It's Tails! You lost!";
+        resultElement.style.color = "#ff4d4d";
+      }
+    }, 2000);
+  });
+  
